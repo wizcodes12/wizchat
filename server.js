c2fa735707414eb251ed30f4d741b00570993718
wizcodes12 <137535009+wizcodes12@users.noreply.github.com>
2024-08-04 12:22:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -14,29 +15,34 @@ const io = socketIo(server, {
   }
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 const users = new Map();
 
 io.on('connection', (socket) => {
   console.log('New client connected');
-
   socket.on('join', (userData) => {
     users.set(socket.id, userData);
     io.emit('userList', Array.from(users.values()));
   });
-
   socket.on('message', (message) => {
     io.emit('message', { ...message, sender: users.get(socket.id) });
   });
-
   socket.on('typing', (isTyping) => {
     socket.broadcast.emit('userTyping', { user: users.get(socket.id), isTyping });
   });
-
   socket.on('disconnect', () => {
     users.delete(socket.id);
     io.emit('userList', Array.from(users.values()));
     console.log('Client disconnected');
   });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
